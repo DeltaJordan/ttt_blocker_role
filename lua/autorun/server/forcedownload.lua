966a6121656d7902c971f217274aad585b6a0333
ChrisScott9456 <christopherscott9456@gmail.com>
2021-12-03 13:35:51 -0500

    Update forcedownload and vtf icon

--- lua/autorun/server/forcedownload.lua
+++ lua/autorun/server/forcedownload.lua
@@ -1,4 +1,4 @@
 -- This file forces clients to download the assets for the weapon (weapon icon).
 -- If you are distributing those files via FastDL, comment out the line below.
 
-resource.AddWorkshop("2666427081")
+resource.AddWorkshop("2671426085")
