7ff64fb9ee69b729094046fac32cf7488a3f098d
Jordan Marine <marine.jordan64@gmail.com>
2025-01-23 15:25:47 -0600

    Use consistent string quotes.

--- lua/terrortown/entities/roles/blocker/shared.lua
+++ lua/terrortown/entities/roles/blocker/shared.lua
@@ -1,23 +1,19 @@
 if SERVER then
 	AddCSLuaFile()
-	resource.AddFile("materials/vgui/ttt/dynamic/roles/icon_block.vmt")
+	resource.AddFile('materials/vgui/ttt/dynamic/roles/icon_block.vmt')
 end
 
 function ROLE:PreInitialize()
 	self.color = Color(112, 62, 62, 255)
-
-	self.abbr = "block"
+	self.abbr = 'block'
 	self.surviveBonus = 0.5 -- bonus multiplier for every survive while another player was killed
 	self.scoreKillsMultiplier = 5 -- multiplier for kill of player of another team
 	self.scoreTeamKillsMultiplier = -16 -- multiplier for teamkill
 	self.preventFindCredits = false
 	self.preventKillCredits = false
 	self.preventTraitorAloneCredits = false
-
 	self.isOmniscientRole = true
-
 	self.defaultTeam = TEAM_TRAITOR
-
 	self.conVarData = {
 		pct = 0.17, -- necessary: percentage of getting this role selected (per player)
 		maximum = 1, -- maximum amount of roles in a round
@@ -36,19 +32,15 @@ end
 
 local function CheckBlockerAlive()
 	for _, ply in ipairs(player.GetAll()) do
-		if ply:GetSubRole() == ROLE_BLOCKER and ply:Alive() then
-			return true
-		end
+		if ply:GetSubRole() == ROLE_BLOCKER and ply:Alive() then return true end
 	end
-
 	return false
 end
 
 if SERVER then
 	hook.Add('TTTCanSearchCorpse', 'TTT2BlockerPreventIdentifyCorpse', function(idPlayer, deadRagdoll, isCovert, isLongRange)
 		-- Prevent any players that aren't the Blocker from identifying bodies while a Blocker is still alive
-		if CheckBlockerAlive()
-		and idPlayer:GetSubRole() != ROLE_BLOCKER then
+		if CheckBlockerAlive() and idPlayer:GetSubRole() ~= ROLE_BLOCKER then
 			idPlayer:PrintMessage(HUD_PRINTTALK, 'Cannot identify bodies while the Blocker is alive!')
 			return false
 		end
@@ -57,12 +49,9 @@ if SERVER then
 	hook.Add('TTT2PostPlayerDeath', 'TTT2BlockerDeath', function(victim, _, _)
 		-- Identify all of the current dead players if all of the Blockers are dead
 		if victim:GetSubRole() == ROLE_BLOCKER and not CheckBlockerAlive() then
-			LANG.MsgAll("ttt2_blocker_role_death", nil, MSG_MSTACK_WARN) -- Show message stating bodies are identified
-
+			LANG.MsgAll('ttt2_blocker_role_death', nil, MSG_MSTACK_WARN) -- Show message stating bodies are identified
 			for _, ply in ipairs(player.GetAll()) do
-				if not ply:Alive() then
-					ply:ConfirmPlayer(true)
-				end
+				if not ply:Alive() then ply:ConfirmPlayer(true) end
 			end
 		end
 	end)
