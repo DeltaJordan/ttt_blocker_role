6783c4419f75b78e664a11dfffa0d900e6c7610f
Jordan Marine <marine.jordan64@gmail.com>
2025-01-23 15:22:53 -0600

    Removed extremely redundant forcedownload (it was downloading itself on the workshop for some reason?).

--- lua/autorun/server/forcedownload.lua
+++
@@ -1,4 +0,0 @@
--- This file forces clients to download the assets for the weapon (weapon icon).
--- If you are distributing those files via FastDL, comment out the line below.
-
-resource.AddWorkshop("2671426085")
